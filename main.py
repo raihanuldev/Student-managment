5fbba3f65a067e7a9d6aa22cdd5e707b23dbabe0
raihanuldev <rihanthedev@gmail.com>
2025-04-12 02:49:46 +0600

    view student info

--- main.py
+++ main.py
@@ -32,16 +32,31 @@ class Student:
         else:
             print(f"⚠️ Student {self._name} is already not enrolled.")
 
+    # Task 6: View student info
+    def view_student_info(self):
+        enrollment_status = "Enrolled" if self._is_enrolled else "Not Enrolled"
+        print(f"Student ID: {self._student_id}")
+        print(f"Name: {self._name}")
+        print(f"Department: {self._department}")
+        print(f"Enrollment Status: {enrollment_status}")
+        print("-" * 30)  # Separator for better readability
 
-# Task 3: Manual
+
+# Task 3: Manual creation of student objects
 student1 = Student("101", "Raihan Shorif", "CSE", True)
 student2 = Student("102", "Shila Song", "EEE", False)
 student3 = Student("103", "John Doe", "BBA", True)
 
+# # Test Enroll/Drop
+# print("\n-- Test Enroll/Drop --")
+# student2.enroll_student()
+# student2.enroll_student()
 
-print("\n-- Test Enroll/Drop --")
-student2.enroll_student()
-student2.enroll_student()  
+# student3.drop_student()
+# student3.drop_student()
 
-student3.drop_student()    
-student3.drop_student()   
+# Test view student info
+print("\n-- Test View Student Info --")
+student1.view_student_info()
+student2.view_student_info()
+student3.view_student_info()
